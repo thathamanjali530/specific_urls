7d9eee40e453c2a3920214ec5d5e44c550d12368
thathamanjali530 <thathamanjali530@gmail.com>
2023-07-21 12:37:06 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def first(request):
+    return HttpResponse('<h1>pelli kaani prasad</h1>')
+
+def second(request):
+    return HttpResponse('prasad will not get married malliswari')
